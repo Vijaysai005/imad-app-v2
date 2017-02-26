6c4167e77a117c44b61a69de85a2e91a44fdecc6
Vijaysai005 <vijay.sai005@gmail.com>
2017-02-26 18:42:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,11 +40,25 @@ var ValueName = InputName.value;
 
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
-    var names = ["name1", "name2", "name3", "name4"];
-    list = " ";
-    for (var i = 0; i < names.length; i++){
-        list += "<li>"+names[i]+"</li>";
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            // Take some action
+            if (request.status === 200){
+                var names = ["name1", "name2", "name3", "name4"];
+                list = " ";
+                for (var i = 0; i < names.length; i++){
+                list += "<li>"+names[i]+"</li>";
     }
     var ul = document.getElementById("namelist");
     ul.innerHTML = list;
+            }
+        }
+    };
+    // make a request
+    request.open('GET','http://vijaysai005.imad.hasura-app.io/submit-name?name='+ValueName, true);
+    request.send(null);
+
 };
