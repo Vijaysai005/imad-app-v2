ddf3f73bd706e8255acdc71c231516b10d6fcd9f
Vijaysai005 <vijay.sai005@gmail.com>
2017-02-26 16:37:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ var element = document.getElementById("main-text");
 
 element.innerHTML = "New value";
 
+// move image
+
+var image = document.getElementById("madi");
+image.onclick = function(){
+    image.style.marginLeft = "100px";
+}
