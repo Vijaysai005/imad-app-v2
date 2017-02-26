0ab3e2d633159b1697c74f938d8a9fdc140ca961
Vijaysai005 <vijay.sai005@gmail.com>
2017-02-26 17:48:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,12 +14,23 @@ image.onclick = function(){
 // Counter
 
 var button = document.getElementById("counter");
-var counter = 0;
 button.onclick = function(){
     
-
-    // Rendering the counter in correct span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readystate === XMLHttpRequest.DONE){
+            // Take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    // make a request
+    request.open('GET','http://vijaysai005.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
